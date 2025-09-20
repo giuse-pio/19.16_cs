c9b61210cadb5c97df436c1c989de9e3e54f3686
giupe <giupe@MSI>
2025-09-20 17:09:50 +0200

    Add project files.

---
+++ 19.16/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _19._16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> premi = new List<int> { 100, 300, 500, 700, 200, 1000};
+            int premiomin;
+            int premiomax;
+            Console.WriteLine("inserisci il numero minimo");
+            while (!int.TryParse(Console.ReadLine(), out premiomax))
+            {
+                Console.WriteLine("Errore");
+            }
+            Console.WriteLine("inserisci il numero massimo");
+            while (!int.TryParse(Console.ReadLine(), out premiomin)) 
+            {
+                Console.WriteLine("Errore");
+            }
+            Console.WriteLine();
+
+            foreach (int premio in premi)
+            {
+                if(premio >= premiomax && premio <= premiomin)
+                {
+                    Console.WriteLine(premio);
+                }
+            }
+        }
+
+    }
+}
